06a7d138fc60798fea3fa47ecb96bb6c190c207e
kalpesh <kalpeshpchaudhari@gmail.com>
2020-10-08 10:39:33 +0530

    FIrst commit

---
+++ src/main/java/com/example/demo/MyDemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/data")
+public class MyDemoController {
+
+    @GetMapping("/hello-world")
+    public ResponseEntity<String> get() {
+
+        return ResponseEntity.ok("Hello World!");
+    }
+
+}
---
+++ src/main/java/com/exception/BusinessException.java
@@ -0,0 +1,9 @@
+package com.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException()
+    {
+        super();
+    }
+
+}
